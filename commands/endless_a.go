c8a20c5cd66c163dd553f94551514d238ecaeafb
Akomd <y.a.b.b.p.4.1.4@gmail.com>
2022-12-02 21:40:20 +0900

    add endless press A script

---
+++ commands/endless_a.go
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+package main
+
+import (
+	"github.com/mzyy94/nscon"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"time"
+)
+
+func setInput(input *uint8) {
+	*input++
+	time.AfterFunc(100*time.Millisecond, func() {
+		*input--
+	})
+}
+
+func main() {
+	target := "/dev/hidg0"
+	con := nscon.NewController(target)
+	con.LogLevel = 2
+	defer con.Close()
+	con.Connect()
+
+	buf := make([]byte, 1)
+
+	// Set tty break for read keyboard input directly
+	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
+	defer exec.Command("stty", "-F", "/dev/tty", "-cbreak").Run()
+	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+	defer exec.Command("stty", "-F", "/dev/tty", "echo").Run()
+
+	go func() {
+		for {
+			os.Stdin.Read(buf)
+            setInput(&con.Input.Button.A)
+            log.Printf("%c: Press A!", time.Now())
+            time.Sleep(time.Millisecond * 250)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	select {
+	case <-c:
+		return
+	}
+
+}
